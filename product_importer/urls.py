bad7c7e0dbd6a725a0cb2dd6a7e1121b8c1a343b
Kshitij <kshitijmoghe10@gmail.com>
2025-11-15 01:13:44 +0530

    added redoc , job check and router link to main viewsets

--- product_importer/urls.py
+++ product_importer/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for product_importer project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .routing import router
+from .docs import schema_view
+from prodhub import views as prodhub_views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), 
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('api/job/<uuid:job_id>/status/', prodhub_views.JobStatusView.as_view(), name='job-status'),
 ]
